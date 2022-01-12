b01eeefa8522733319adb514db54045515e824e0
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-12 06:32:40 +0200

    ✨ expose url for the first version of the fibonacci sequence api

--- coding_task/apis/urls.py
+++ coding_task/apis/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from coding_task.apis.views import FibonacciSequenceV1APIView
+
 
 app_name = "apis"
-urlpatterns = []
+urlpatterns = [
+    path("fibonacci-sequence/v1/<str:n>/", FibonacciSequenceV1APIView.as_view(), name="fibonacci-sequence-v1"),
+]
