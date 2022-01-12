8cb95ab7ddfc32ff775d74ec1de053a9d8d36d11
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-13 00:29:09 +0200

    ♻️  remove extra comments, as leaving it only at the first test case for demonstration

--- coding_task/apis/tests/test_serializers.py
+++ coding_task/apis/tests/test_serializers.py
@@ -31,28 +31,22 @@ class TestInputSerializer:
     def test__inputserializer__with_invalid_n__should_raise_validation_error(
         self, dummy_string: str
     ) -> None:
-        # Arrange
         serializer = InputSerializer(data={"n": dummy_string})
 
-        # Act
         with pytest.raises(exceptions.ValidationError) as err_info:
             serializer.is_valid(raise_exception=True)
 
-        # Assert
         assert err_info.type == exceptions.ValidationError
         assert err_info.value.args[0]["n"][0] == "A valid integer is required."
 
     def test__inputserializer__with_negative_n__should_raise_validation_error(
         self,
     ) -> None:
-        # Arrange
         serializer = InputSerializer(data={"n": -1})
 
-        # Act
         with pytest.raises(exceptions.ValidationError) as err_info:
             serializer.is_valid(raise_exception=True)
 
-        # Assert
         assert err_info.type == exceptions.ValidationError
         assert (
             err_info.value.args[0]["n"][0]
@@ -64,31 +58,25 @@ class TestInputSerializer:
         n_random_input_number: int,
         m_random_input_number: int,
     ) -> None:
-        # Arrange
         serializer = InputSerializer(
             data={"n": n_random_input_number, "m": m_random_input_number}
         )
 
-        # Act
         serializer.is_valid(raise_exception=True)
 
-        # Assert
         assert serializer.validated_data["n"] == n_random_input_number
         assert serializer.validated_data["m"] == m_random_input_number
 
     def test__inputserializer__with_invalid_m__should_raise_validation_error(
         self, n_random_input_number: int, dummy_string: str
     ) -> None:
-        # Arrange
         serializer = InputSerializer(
             data={"n": n_random_input_number, "m": dummy_string}
         )
 
-        # Act
         with pytest.raises(exceptions.ValidationError) as err_info:
             serializer.is_valid(raise_exception=True)
 
-        # Assert
         assert err_info.type == exceptions.ValidationError
         assert err_info.value.args[0]["m"][0] == "A valid integer is required."
 
@@ -96,14 +84,11 @@ class TestInputSerializer:
         self,
         n_random_input_number: int,
     ) -> None:
-        # Arrange
         serializer = InputSerializer(data={"n": n_random_input_number, "m": -1})
 
-        # Act
         with pytest.raises(exceptions.ValidationError) as err_info:
             serializer.is_valid(raise_exception=True)
 
-        # Assert
         assert err_info.type == exceptions.ValidationError
         assert (
             err_info.value.args[0]["m"][0]
