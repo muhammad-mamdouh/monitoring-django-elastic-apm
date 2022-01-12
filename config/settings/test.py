0f841e88aa6794e7672b16799465e579572a04e6
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-12 06:44:33 +0200

    🔧 add secret key configs to the test settings module

--- config/settings/test.py
+++ config/settings/test.py
@@ -4,5 +4,12 @@ With these settings, tests run faster.
 
 from .base import *  # noqa
 
+# GENERAL
+# ------------------------------------------------------------------------------
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="o&9-xt)9o3aj28&hs=1i_x1dpfos^@8rkz%r-w6qr86m*hrsdb",
+)
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
