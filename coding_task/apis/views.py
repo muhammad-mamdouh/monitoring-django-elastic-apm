344dcf431b76a09d92448bd9f2aafaefbda58c0b
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-12 06:34:44 +0200

    ✨ add v2 for the nth fibonacci sequence api endpoint

--- coding_task/apis/views.py
+++ coding_task/apis/views.py
@@ -8,6 +8,8 @@ from coding_task.apis.serializers import NInputSerializer
 class FibonacciSequenceV1APIView(APIView):
     """
     API endpoint to return the nth term of Fibonacci Sequence.
+    Version 1.
+
     :param int n: term for which the api will calculate the nth fibonacci sequence.
     :return int fibonacci_sequence: nth term of fibonacci sequence regarding the passed number.
     """
@@ -29,9 +31,29 @@ class FibonacciSequenceV1APIView(APIView):
             result = self.fibonacci_sequence(serializer.validated_data["n"])
         except Exception as e:
             # TODO: log the error
-            return Response({
-                "error": "Sorry, we're facing internal errors 🤯 please try again!"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return Response(
+                {"error": "Sorry, we're facing internal errors 🤯 please try again!"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
         return Response({"fibonacci_sequence": result}, status=status.HTTP_200_OK)
+
+
+class FibonacciSequenceV2APIView(APIView):
+    """
+    API endpoint to return the nth term of Fibonacci Sequence.
+    Version 2.
+    Under development, please use version 1.
+
+    :param int n: term for which the api will calculate the nth fibonacci sequence.
+    :return int fibonacci_sequence: nth term of fibonacci sequence regarding the passed number.
+    """
+
+    def get(self, request, *args, **kwargs):
+
+        return Response(
+            {
+                "notice": "Version 2 is still under development 💻 please use version 1."
+            },
+            status=status.HTTP_503_SERVICE_UNAVAILABLE,
+        )
