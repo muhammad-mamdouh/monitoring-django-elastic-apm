1c8c329e943aba0e25703ebf581a0dd50ebf2a20
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-12 05:41:07 +0200

    ✨ add the django extensions package to the local installed apps

--- config/settings/local.py
+++ config/settings/local.py
@@ -9,3 +9,8 @@ SECRET_KEY = env(
     default="1ZDUIHYgHDmOob3lpmtDWkr2Im6cp2UpUgdx5lMTXJipJGXByKdm8ZpYGREcJui3",
 )
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+# django-extensions
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
+INSTALLED_APPS += ["django_extensions"]  # noqa F405
