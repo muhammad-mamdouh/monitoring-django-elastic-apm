210a16032b0b6691670c3510a0c7f773193edefb
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-12 06:43:32 +0200

    ♻️  change the views module into a package to separate the different apis

---
+++ coding_task/apis/views/__init__.py
@@ -0,0 +1,7 @@
+from coding_task.apis.views.fibonacci_api_views import (
+    FibonacciSequenceV1APIView,
+    FibonacciSequenceV2APIView,
+)
+
+
+__all__ = ["FibonacciSequenceV1APIView", "FibonacciSequenceV2APIView"]
