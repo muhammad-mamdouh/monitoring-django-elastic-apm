36e25c1ae3f249e6c0f04ecca3cf1d2d18b8e474
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-01-12 07:08:25 +0200

    ✨ add new field to the input serializer to handle both m & n values

--- coding_task/apis/serializers.py
+++ coding_task/apis/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 
 class InputSerializer(serializers.Serializer):
     """
-    Input serializer to receive and validate the n value.
+    Input serializer to receive and validate the m and n values.
     """
 
     n = serializers.IntegerField(min_value=0)
+    m = serializers.IntegerField(min_value=0, required=False)
